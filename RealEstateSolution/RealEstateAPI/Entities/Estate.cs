c07a6e88fc59c3bf4baf32508fcab7f35bc163e9
Novak Bubanja <42568713+bubanjan@users.noreply.github.com>
2025-02-14 23:43:23 +0100

    added sql lite db

---
+++ RealEstateSolution/RealEstateAPI/Entities/Estate.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstateAPI.Entities
+{
+    public class Estate
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public int? Price { get; set; }
+        public int Size { get; set; }
+    }
+}
---
+++ RealEstateSolution/RealEstateAPI/Migrations/20250214223550_Init.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Estates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<int>(type: "INTEGER", nullable: true),
+                    Size = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estates", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Estates");
+        }
+    }
+}
