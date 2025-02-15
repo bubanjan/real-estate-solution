0b4322174edfb357386e6b990588d458c950a8c8
Novak Bubanja <novak.bubanja@goteborgsregionen.se>
2025-02-15 20:28:20 +0100

    added delete estate endpoint

--- RealEstateSolution/RealEstateAPI/Repositories/IRealEstateRepository.cs
+++ RealEstateSolution/RealEstateAPI/Repositories/IRealEstateRepository.cs
@@ -5,6 +5,11 @@ namespace RealEstateAPI.Repositories
     public interface IRealEstateRepository
     {
         Task<IEnumerable<EstateDto>> GetEstatesAsync();
+
         Task<EstateDto?> GetEstateAsync(int estateId);
+
+        Task<bool> SaveChangesAsync();
+
+        Task<bool> DeleteEstateAsync(int estateId);
     }
 }
