6489578d74ff426ac92c6482654e84c1d5ea376d
Novak Bubanja <novak.bubanja@goteborgsregionen.se>
2025-02-16 14:26:01 +0100

    added Title and City property

---
+++ RealEstateSolution/RealEstateAPI/Migrations/20250216125909_CityAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CityAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "City",
+                table: "Estates",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Estates");
+        }
+    }
+}
--- RealEstateSolution/RealEstateAPI/Models/EstateDto.cs
+++ RealEstateSolution/RealEstateAPI/Models/EstateDto.cs
@@ -5,9 +5,11 @@ namespace RealEstateAPI.Models
     public class EstateDto
     {
         public int Id { get; set; }
+        public string Title { get; set; }
         public string? Description { get; set; }
         public int? Price { get; set; }
         public int Size { get; set; }
         public EstateType EstateCategory { get; set; }
+        public City City { get; set; }
     }
 }
