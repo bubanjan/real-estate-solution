f4f87bb033525b9c3e1296deb18d90f0443e533d
Novak Bubanja <novak.bubanja@goteborgsregionen.se>
2025-02-21 23:29:47 +0100

    users table added

---
+++ RealEstateSolution/RealEstateAPI/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealEstateAPI.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string PasswordSalt { get; set; }
+
+        public string PasswordHash { get; set; }
+    }
+}
