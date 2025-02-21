098780c4f953064b69c969da54a81c5fed6ac5f5
Novak Bubanja <42568713+bubanjan@users.noreply.github.com>
2025-02-22 00:44:56 +0100

    jwt authentication added

---
+++ RealEstateSolution/RealEstateAPI/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using RealEstateAPI.Entities;
+
+namespace RealEstateAPI.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetUserByUsernameAsync(string username);
+
+        Task<User?> GetUserByEmailAsync(string email);
+
+        Task<User?> GetUserByIdAsync(int userId);
+
+        void AddUser(User user);
+
+        Task<bool> SaveChangesAsync();
+    }
+}
