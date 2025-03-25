7896ec45ff42b7687a73ce14c680b08e9b89c309
Novak Bubanja <novak.bubanja@goteborgsregionen.se>
2025-03-25 22:14:18 +0100

    Tag Entity and Controller added

---
+++ RealEstateSolution/RealEstateAPI/Entities/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealEstateAPI.Entities
+{
+    public class Tag
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<Estate> Estates { get; set; } = new List<Estate>();
+    }
+}
---
+++ RealEstateSolution/RealEstateAPI/Migrations/20250325205617_UpdateTagSeed.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RealEstateAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTagSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EstateTags",
+                columns: table => new
+                {
+                    EstatesId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TagsId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EstateTags", x => new { x.EstatesId, x.TagsId });
+                    table.ForeignKey(
+                        name: "FK_EstateTags_Estates_EstatesId",
+                        column: x => x.EstatesId,
+                        principalTable: "Estates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EstateTags_Tags_TagsId",
+                        column: x => x.TagsId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tags",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "SeaView" },
+                    { 2, "NewBuild" },
+                    { 3, "HasParking" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EstateTags_TagsId",
+                table: "EstateTags",
+                column: "TagsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EstateTags");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
