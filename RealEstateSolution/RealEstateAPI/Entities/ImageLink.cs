28044a99cc0ea2589cf203136c7f0fe8cb4752f5
Novak Bubanja <42568713+bubanjan@users.noreply.github.com>
2025-03-29 14:20:24 +0100

    added imageLink entity

---
+++ RealEstateSolution/RealEstateAPI/Entities/ImageLink.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstateAPI.Entities
+{
+    public class ImageLink
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+
+        public int EstateId { get; set; }
+        public Estate Estate { get; set; }
+    }
+}
---
+++ RealEstateSolution/RealEstateAPI/Migrations/20250327201511_AddedImageLinks.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedImageLinks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ImageLinks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Url = table.Column<string>(type: "TEXT", nullable: false),
+                    RealEstateId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ImageLinks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ImageLinks_Estates_RealEstateId",
+                        column: x => x.RealEstateId,
+                        principalTable: "Estates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ImageLinks_RealEstateId",
+                table: "ImageLinks",
+                column: "RealEstateId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ImageLinks");
+        }
+    }
+}
