06506354318b5a47767e5ea47bc4a5eea1a97ad4
Novak Bubanja <42568713+bubanjan@users.noreply.github.com>
2025-04-01 22:48:08 +0200

    added max lenghts to string properties in Estate

--- RealEstateSolution/RealEstateAPI/Entities/Estate.cs
+++ RealEstateSolution/RealEstateAPI/Entities/Estate.cs
@@ -9,13 +9,19 @@ namespace RealEstateAPI.Entities
         public int Id { get; set; }
 
         [Required]
+        [MaxLength(300)]
         public string Title { get; set; } = string.Empty;
 
+        [MaxLength(2500)]
         public string? Description { get; set; }
+
         public int? Price { get; set; }
         public int Size { get; set; }
         public EstateType EstateCategory { get; set; }
+
+        [MaxLength(300)]
         public string? SellerContact { get; set; }
+
         public City City { get; set; }
 
         public ICollection<Tag> Tags { get; set; } = new List<Tag>();
