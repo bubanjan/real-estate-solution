91e47bfb3b16de4d8c16a721ca1617ba2ccf7434
Novak Bubanja <Novak.Bubanja@goteborgsregionen.se>
2025-05-04 23:26:48 +0200

    fixed css code

--- RealEstateSolution/RealEstateFrontend/src/App.jsx
+++ RealEstateSolution/RealEstateFrontend/src/App.jsx
@@ -1,15 +1,26 @@
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './pages/Home'
+import { CssBaseline, Box } from '@mui/material'
 
 export default function App() {
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* TO DO: ADD About Us, Our Team, Contact..*/}
-      </Routes>
+      <CssBaseline />
+      <Box
+        sx={{
+          /*  backgroundColor: '#000',
+           color: '#00ffcc',
+           minHeight: '100vh',
+           fontFamily: '"Press Start 2P", monospace', */
+        }}
+      >
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* TO DO: ADD About Us, Our Team, Contact..*/}
+        </Routes>
+      </Box>
     </>
   )
 }
