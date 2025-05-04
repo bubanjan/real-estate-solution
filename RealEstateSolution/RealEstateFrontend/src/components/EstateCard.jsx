3361964380483861982270c8eade5ea106b2f49b
Novak Bubanja <42568713+bubanjan@users.noreply.github.com>
2025-05-04 22:07:32 +0200

    wip

--- RealEstateSolution/RealEstateFrontend/src/components/EstateCard.jsx
+++ RealEstateSolution/RealEstateFrontend/src/components/EstateCard.jsx
@@ -1,16 +1,20 @@
 import { Card, CardMedia, CardContent, Typography } from '@mui/material'
+import noImage from '../assets/noImage.png'
 
 export default function EstateCard({ estate }) {
   const trimDescription = (text, maxLength = 200) =>
     text.length > maxLength ? text.substring(0, maxLength) + '...' : text
+
+  const imageUrl = estate.imageUrl || noImage
 
   return (
     <Card sx={{ cursor: 'pointer' }}>
       <CardMedia
         component="img"
         height="200"
-        image={estate.imageUrl}
-        alt={estate.title}
+        image={imageUrl}
+        alt={estate.title || 'No Image Available'}
+        sx={{ objectFit: 'contain', backgroundColor: '#f0f0f0' }}
       />
       <CardContent>
         <Typography variant="h6">{estate.title}</Typography>
