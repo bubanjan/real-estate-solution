ca51f8c6e534cadfd742b7ec94076ac17d57f0ea
Novak Bubanja <Novak.Bubanja@goteborgsregionen.se>
2025-05-09 22:47:05 +0200

    cleaning

--- RealEstateSolution/RealEstateFrontend/src/components/EstateCard.jsx
+++ RealEstateSolution/RealEstateFrontend/src/components/EstateCard.jsx
@@ -9,20 +9,20 @@ export default function EstateCard({ estate }) {
 
   return (
     <Card sx={{ cursor: 'pointer' }}>
-      <CardMedia
+      {/*   <CardMedia
         component="img"
         height="200"
         image={imageUrl}
         alt={estate.title || 'No Image Available'}
         sx={{ objectFit: 'contain', backgroundColor: '#f0f0f0' }}
-      />
+      /> */}
       <CardContent>
         <Typography variant="h6">{estate.title}</Typography>
         <Typography variant="body2" color="text.secondary">
           {trimDescription(estate.description)}
         </Typography>
-        <Typography mt={1}>💰 {estate.price} EUR</Typography>
-        <Typography>📐 {estate.size} m²</Typography>
+        <Typography mt={1}> {estate.price} EUR</Typography>
+        <Typography> {estate.size} m²</Typography>
       </CardContent>
     </Card>
   )
