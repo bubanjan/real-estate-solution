6f2a2fe18ab1c23833a99730d9d680be8e2b6eba
Novak Bubanja <Novak.Bubanja@goteborgsregionen.se>
2025-05-11 20:32:29 +0200

    borderRadius added

--- RealEstateFrontend/src/components/SearchBar.jsx
+++ RealEstateFrontend/src/components/SearchBar.jsx
@@ -32,20 +32,27 @@ export default function SearchBar({
 
             <Stack direction="row" spacing={2} sx={{ flexWrap: 'wrap', width: '100%', justifyContent: 'center' }}>
                 <TextField
+                    
+                    size="small"
                     variant="outlined"
                     placeholder="Search estates..."
                     value={value}
                     onChange={(e) => setValue(e.target.value)}
                     onKeyDown={handleKeyDown}
-                    sx={{ flex: 1, minWidth: 200, maxWidth: 290}} 
+                    sx={{ flex: 1, minWidth: 200, maxWidth: 290, '& .MuiOutlinedInput-root': {
+                    borderRadius: 2,
+                },}} 
                 />
 
                 <TextField
+                    size="small"
                     select
                     label="City"
                     onChange={(e) => onCityChange(e.target.value)}
                     defaultValue=""
-                    sx={{ flex: 1, minWidth: 200, maxWidth: 270 }}
+                    sx={{ flex: 1, minWidth: 200, maxWidth: 270, '& .MuiOutlinedInput-root': {
+                    borderRadius: 2,
+                } }}
                 >
                     {cities.map((c) => (
                         <MenuItem key={c.value} value={c.value}>{c.label}</MenuItem>
@@ -53,11 +60,14 @@ export default function SearchBar({
                 </TextField>
 
                 <TextField
+                    size="small"
                     select
                     label="Estate Type"
                     onChange={(e) => onTypeChange(e.target.value)}
                     defaultValue=""
-                    sx={{ flex: 1, minWidth: 200, maxWidth: 270 }}
+                    sx={{ flex: 1, minWidth: 200, maxWidth: 270, '& .MuiOutlinedInput-root': {
+                    borderRadius: 2,
+                } }}
                 >
                     {estateTypes.map((t) => (
                         <MenuItem key={t.value} value={t.value}>{t.label}</MenuItem>
@@ -66,16 +76,31 @@ export default function SearchBar({
             </Stack>
 
             <Stack direction="row" spacing={2} sx={{ flexWrap: 'wrap', width: '100%', justifyContent: 'center' }}>
-                <TextField type="number" label="Min Price" onChange={(e) => onMinPriceChange(e.target.value)} sx={{ flex: 1, minWidth: 150, maxWidth: 150 }} />
-                <TextField type="number" label="Max Price" onChange={(e) => onMaxPriceChange(e.target.value)} sx={{ flex: 1, minWidth: 150, maxWidth: 150 }} />
-                <TextField type="number" label="Min Size" onChange={(e) => onMinSizeChange(e.target.value)} sx={{ flex: 1, minWidth: 150, maxWidth: 150 }} />
-                <TextField type="number" label="Max Size" onChange={(e) => onMaxSizeChange(e.target.value)} sx={{ flex: 1, minWidth: 150, maxWidth: 150 }} />
+                <TextField size="small" type="number" label="Min Price" onChange={(e) => onMinPriceChange(e.target.value)} 
+                sx={{ flex: 1, minWidth: 150, maxWidth: 150, '& .MuiOutlinedInput-root': {
+                    borderRadius: 2,
+                } }} />
+                <TextField size="small" type="number" label="Max Price" onChange={(e) => onMaxPriceChange(e.target.value)} 
+                sx={{ flex: 1, minWidth: 150, maxWidth: 150, '& .MuiOutlinedInput-root': {
+                    borderRadius: 2,
+                } }} />
+                <TextField size="small" type="number" label="Min Size" onChange={(e) => onMinSizeChange(e.target.value)} 
+                sx={{ flex: 1, minWidth: 150, maxWidth: 150, '& .MuiOutlinedInput-root': {
+                    borderRadius: 2,
+                } }} />
+                <TextField size="small" type="number" label="Max Size" onChange={(e) => onMaxSizeChange(e.target.value)} 
+                sx={{ flex: 1, minWidth: 150, maxWidth: 150, '& .MuiOutlinedInput-root': {
+                    borderRadius: 2,
+                } }} />
                 <TextField
+                    size="small"
                     select
                     label="Order By"
                     onChange={(e) => onOrderChange(e.target.value)}
                     defaultValue=""
-                    sx={{ flex: 1, minWidth: 200, maxWidth: 200 }}
+                    sx={{ flex: 1, minWidth: 200, maxWidth: 200, '& .MuiOutlinedInput-root': {
+                    borderRadius: 2,
+                } }}
                 >
                     {orderOptions.map((o) => (
                         <MenuItem key={o.value} value={o.value}>{o.label}</MenuItem>
